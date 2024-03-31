7fba172865bf39d96a96084332bcd979671e925a
Denis Irz <denirz@iMac-Denirz.local>
2024-03-31 13:54:48 +0300

    hatch init

---
+++ ntf-zbx/src/ntf_zbx/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Denis Irz <denirz@gmail.com>
+#
+# SPDX-License-Identifier: MIT
---
+++ ntf-zbx/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Denis Irz <void@some.where>
+#
+# SPDX-License-Identifier: MIT
